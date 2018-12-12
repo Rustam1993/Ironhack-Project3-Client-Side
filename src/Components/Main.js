da01d628ba3fecbe29fd69b015c4e34947f4272c
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-12 12:07:16 -0500

    set up react tools to build front end

---
+++ src/Components/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+import {Link} from 'react-router-dom';
+
+class Main extends Component{
+
+
+
+render(){
+
+    return(
+        <div>
+       <Link to = '/signup-user' > Sign up </Link>
+       </div>
+    )
+
+}
+
+}
+
+
+export default Main;
