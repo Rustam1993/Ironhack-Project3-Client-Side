060dbbd6474bece2690d372dd1ca142165384f23
Rachel Hofer <rhofer3@gmail.com>
2018-12-16 14:02:32 -0500

    profile page styled

--- src/Components/UserComp/UserProfile.js
+++ src/Components/UserComp/UserProfile.js
@@ -27,35 +27,11 @@ class UserProfile extends Component{
             this.setState({
                 currentUser : userFromDb
             })
-            // this.showAllUserInfo();
-            // console.log("CURRENT", this.state.currentUser)
         })
 
     }
 
-    // showAllUserInfo = () => {
-    //     if(this.state.currentUser){
-    //         return(
-    //             <div class="card" style="width: 18rem;">
-    //                 <img class="card-img-top" src={this.state.currentUser.profilePic} alt="Card image cap"/>
-    //                 <div class="card-body">
-    //                     <h5 class="card-title">{this.state.currentUser.fullName}</h5>
-    //                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    //                 </div>
-    //                 <ul class="list-group list-group-flush">
-    //                     <li class="list-group-item">{this.state.currentUser.zipCode}</li>
-    //                     <li class="list-group-item">{this.state.currentUser.email}</li>
-    //                 </ul>
-    //                 <div class="card-body">
-    //                     <a href="#" class="card-link">Card link</a>
-    //                     <a href="#" class="card-link">Another link</a>
-    //                 </div>
-    //             </div>
-    //         )
-    //     }
-    // }
 
-    
     deleteProperty = (propertyID) => {
         this.serviceProperty.deleteProperty(propertyID)
         .then((deletedProperty)=>{
@@ -73,6 +49,28 @@ class UserProfile extends Component{
         })
     }
 
+    showProfile = () => {
+        if(this.state.currentUser){
+            return(
+            <div class="card addedProfileStyle">
+            <h4 className="propertiesCreated">Profile</h4>
+                <img class="card-img-top" src={this.state.currentUser.image} alt="Card image cap"/>
+                
+                    <div class="card-body">
+                        <h5 class="card-title">{this.state.currentUser.fullName}</h5>
+                        <p class="card-text">Ask Rustam if we want to add a BIO in the edit Profile view</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Zip Code: {this.state.currentUser.zipCode}</li>
+                        <li class="list-group-item">E-mail: {this.state.currentUser.email}</li>
+                    </ul>
+                    <div class="card-body">
+                        <Link class="card-link" to = {'/edit-profile/' + this.state.currentUser._id }> Edit profile</Link>
+                    </div>
+                </div>
+            )
+        }
+    }
 
     showListedProperties = () =>{
         if(this.state.currentUser){
@@ -80,7 +78,7 @@ class UserProfile extends Component{
             return (
 
                 <div id="carouselExampleControls" class="carousel slide masterCarouselStyle" data-ride="carousel" key={Index}>
-                        
+                        <h4 className="propertiesCreated">Created Properties</h4>
                     <div class="carousel-inner">
 
                         <div class="carousel-item active">
@@ -93,13 +91,33 @@ class UserProfile extends Component{
                                 </div>
                         </div>
 
+                        <div class="carousel-item">
+                            <img class="d-block w-100 carouselImageStyle" src={element.image} alt="First slide"/>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{element.address}</h5>
+                                    <p>{element.features}</p>
+                                    <Link to={'/edit-property/'+ element._id}>Edit Property</Link><br></br>
+                                    <button onClick={()=> this.deleteProperty(element._id)} className="delete">Delete Property</button>
+                                </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <img class="d-block w-100 carouselImageStyle" src={element.image} alt="First slide"/>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{element.address}</h5>
+                                    <p>{element.features}</p>
+                                    <Link to={'/edit-property/'+ element._id}>Edit Property</Link><br></br>
+                                    <button onClick={()=> this.deleteProperty(element._id)} className="delete">Delete Property</button>
+                                </div>
+                        </div>
+
                     </div>
 
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <a id="right_button" class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <a id="left_button" class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
                         </a>       
@@ -111,42 +129,96 @@ class UserProfile extends Component{
         return (
 
             <div>
-            <div class="card addedProfileStyle">
-                <img class="card-img-top" src={this.state.currentUser.image} alt="Card image cap"/>
-                <div class="card-body">
-                    <h5 class="card-title">{this.state.currentUser.fullName}</h5>
-                    <p class="card-text">Ask Rustam if we want to add a BIO in the edit Profile view</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Zip Code: {this.state.currentUser.zipCode}</li>
-                    <li class="list-group-item">E-mail: {this.state.currentUser.email}</li>
-                </ul>
-                <div class="card-body">
-                    <Link class="card-link" to = {'/edit-profile/' + this.state.currentUser._id }> Edit profile</Link>
-                </div>
-                </div>
-                <h3>Properties created: </h3>
                 {array}
-
-            </div>
-
-
-            
-             
-            
-
-          
+            </div> 
         )
-        }
+    }
+    }
+
+    showViewedProperties = () =>{
+        if(this.state.currentUser){
+            let array = this.state.currentUser.propertiesCreated.map((element,Index) =>{
+            return (
+
+                <div id="carouselExampleControls" class="carousel slide masterCarouselStyle" data-ride="carousel" key={Index}>
+                        <h4 className="propertiesCreated">Viewed Properties</h4>
+                    <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+                            <img class="d-block w-100 carouselImageStyle" src={element.image} alt="First slide"/>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{element.address}</h5>
+                                    <p>{element.features}</p>
+                                    <Link to={'/edit-property/'+ element._id}>Edit Property</Link><br></br>
+                                    <button onClick={()=> this.deleteProperty(element._id)} className="delete">Delete Property</button>
+                                </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <img class="d-block w-100 carouselImageStyle" src={element.image} alt="First slide"/>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{element.address}</h5>
+                                    <p>{element.features}</p>
+                                    <Link to={'/edit-property/'+ element._id}>Edit Property</Link><br></br>
+                                    <button onClick={()=> this.deleteProperty(element._id)} className="delete">Delete Property</button>
+                                </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <img class="d-block w-100 carouselImageStyle" src={element.image} alt="First slide"/>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{element.address}</h5>
+                                    <p>{element.features}</p>
+                                    <Link to={'/edit-property/'+ element._id}>Edit Property</Link><br></br>
+                                    <button onClick={()=> this.deleteProperty(element._id)} className="delete">Delete Property</button>
+                                </div>
+                        </div>
+
+                    </div>
+
+                        <a id="right_button" class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a id="left_button" class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>       
+                </div> 
+
+            )
+        });
+
+        return (
+
+            <div>
+                {array}
+            </div> 
+        )
+    }
     }
 
     render(){
             console.log(this.state.currentUser)
         return(
 
+           
             <div>
-                 {this.showListedProperties()}
+
+                <div className="profile">
+                    {this.showProfile()}
+                </div>
+
+                 <div className="listedProps">
+                    {this.showListedProperties()}
+                 </div>
+
+                 <div className="viewedProps">
+                    {this.showViewedProperties()}
+                 </div>
+
              </div>
+
         )
     }
 
