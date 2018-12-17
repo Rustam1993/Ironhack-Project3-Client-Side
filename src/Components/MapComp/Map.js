c6a938f314771fe2419faa47856f8cfe1c26f6b0
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-17 09:12:04 -0500

    added google map for react

---
+++ src/Components/MapComp/Map.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+
+
+
+
+
+
+ class GoogleMapsContainer extends Component{
+
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {}
+      }
+
+    render(){
+
+        const style = {
+            width: '50vw',
+            height: '75vh',
+            'marginLeft': 'auto',
+            'marginRight': 'auto'
+          }
+
+          return(
+            <Map
+            item
+            xs = { 12 }
+            style = { style }
+            google = { this.props.google }
+            
+            zoom = { 14 }
+            initialCenter = {{ lat: 39.648209, lng: -75.711185 }}
+          />
+          )
+         
+    }
+}
+
+export default GoogleApiWrapper({
+    api: (process.env.googleMapsAPI)
+})(GoogleMapsContainer)
